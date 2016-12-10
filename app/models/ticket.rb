88fc6ca5a4f20fe6687a88e86b56e88346e7e36f
bojkov@mbconsalting.ru <bojkov@mbconsalting.ru>
2016-12-10 17:45:55 +0300

    У билета заданы обязательные поля

--- app/models/ticket.rb
+++ app/models/ticket.rb
@@ -1,8 +1,8 @@
 class Ticket < ApplicationRecord
   validates :number, presence: true
 
-  belongs_to :train, optional: true
-  belongs_to :user, optional: true
-  belongs_to :start_station, class_name: 'RailwayStation', foreign_key: :start_station_id, optional: true
-  belongs_to :end_station, class_name: 'RailwayStation', foreign_key: :end_station_id, optional: true
+  belongs_to :train
+  belongs_to :user
+  belongs_to :start_station, class_name: 'RailwayStation'
+  belongs_to :end_station, class_name: 'RailwayStation'
 end
